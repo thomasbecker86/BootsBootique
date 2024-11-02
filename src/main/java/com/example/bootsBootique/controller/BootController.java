62df6a16c1e5b6c51cac31938898d4779607c3f2
Thomas <thomasbecker1986@gmail.com>
2024-11-02 11:05:22 +0100

    Implemented increment and decrement; code clean up

--- src/main/java/com/example/bootsBootique/controller/BootController.java
+++ src/main/java/com/example/bootsBootique/controller/BootController.java
@@ -13,23 +13,16 @@ import com.example.bootsBootique.enums.BootType;
 import com.example.bootsBootique.model.Boot;
 import com.example.bootsBootique.repository.BootRepository;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.Arrays;
 import java.util.Optional;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -52,70 +45,46 @@ public class BootController {
         model.addAttribute("searchBoot", emptyBoot);
         return "index";
     }
-    /*
-    @GetMapping("/types")
-    public List<BootType> getBootTypes() {
-	return Arrays.asList(BootType.values());
-    }
-    */
+
     @PostMapping("/add")
     public String addBoot(Model model, @ModelAttribute("newBoot") Boot newBoot) {
         this.bootRepository.save(newBoot);
-        System.out.println(newBoot);
-        
-        Iterable<Boot> boots = this.bootRepository.findAll();
-        model.addAttribute("boots", boots);
-        model.addAttribute("listType", BootType.values());
-        Boot emptyBoot = new Boot();
-        model.addAttribute("newBoot", emptyBoot);
-        model.addAttribute("searchBoot", emptyBoot);
-        return "index";
-    }
-    /*
-    @DeleteMapping("/{id}")
-    public Boot deleteBoot(@PathVariable("id") Integer id) {
-        Optional<Boot> bootToDeleteOptional = this.bootRepository.findById(id);
-        if (!bootToDeleteOptional.isPresent()) {
-            return null;
-        }
-        Boot bootToDelete = bootToDeleteOptional.get();
-        this.bootRepository.delete(bootToDelete);
-        return bootToDelete;
+        System.out.println(newBoot);        
+        return "redirect:/boots/";
     }
     
-    @PutMapping("/{id}/quantity/increment")
-    public Boot incrementQuantity(@PathVariable("id") Integer id) {
-        System.out.println("Testing");
-        Optional<Boot> bootToIncrementOptional = this.bootRepository.findById(id);
-        if (!bootToIncrementOptional.isPresent()) {
-            return null;
+    @GetMapping("/delete/{id}")
+    public String deleteBoot(Model model, @PathVariable("id") Integer id) throws QueryNotSupportedException {        
+        Optional<Boot> bootToDeleteOptional = this.bootRepository.findById(id);
+        if (!bootToDeleteOptional.isPresent()) {
+            throw new QueryNotSupportedException("Couldn't find a boot with the given id!");
         }
-        Boot bootToIncrement = bootToIncrementOptional.get();
-        bootToIncrement.setQuantity(bootToIncrement.getQuantity() + 1);
-        return this.bootRepository.save(bootToIncrement);
-    }
+        this.bootRepository.delete(bootToDeleteOptional.get());
+        return "redirect:/boots/";
+    }    
 
-    @PutMapping("/{id}/quantity/decrement")
-    public Boot decrementQuantity(@PathVariable("id") Integer id) {
-        Optional<Boot> bootToDecrementOptional = this.bootRepository.findById(id);
-        if (!bootToDecrementOptional.isPresent()) {
-            return null;
+    @GetMapping("/changequant/{id}/{change}")
+    public String changeQuantity(Model model, @PathVariable("id") Integer id, @PathVariable("change") String change) throws QueryNotSupportedException {
+        Optional<Boot> bootToChangeQuantityOptional = this.bootRepository.findById(id);
+        if (!bootToChangeQuantityOptional.isPresent()) {
+            throw new QueryNotSupportedException("Couldn't find a boot with the given id!");
         }
-        Boot bootToDecrement = bootToDecrementOptional.get();
-        bootToDecrement.setQuantity(bootToDecrement.getQuantity() - 1);
-        return this.bootRepository.save(bootToDecrement);
+        Boot bootToChangeQuantity = bootToChangeQuantityOptional.get();
+        if (change.equalsIgnoreCase("increment")) {
+            bootToChangeQuantity.setQuantity(bootToChangeQuantity.getQuantity() + 1);
+        } else if (change.equalsIgnoreCase("decrement")) {
+            if (bootToChangeQuantity.getQuantity() > 1) {
+                bootToChangeQuantity.setQuantity(bootToChangeQuantity.getQuantity() - 1);
+            }
+        } else {
+            throw new QueryNotSupportedException("An error occured whily trying to change the quantity of the boot with id " + id + "!");
+        }        
+        this.bootRepository.save(bootToChangeQuantity);        
+        return "redirect:/boots/";
     }
-    */
     
     @GetMapping("/search")
     public String searchBoots(Model model, @ModelAttribute("searchBoot") Boot searchBoot) throws QueryNotSupportedException {
-        System.out.println(searchBoot.toString());
-        System.out.println(searchBoot.getType() == null);
-        System.out.println(Objects.isNull(searchBoot.getMaterial()));
-        System.out.println(searchBoot.getMaterial() == null);
-        System.out.println(searchBoot.getMaterial().equals(""));
-        
-        //Iterable<Boot> boots = this.bootRepository.findByMaterial("leather");
         Iterable<Boot> boots = new ArrayList<>();
         String material = searchBoot.getMaterial();
         Float size = searchBoot.getSize();
@@ -131,13 +100,13 @@ public class BootController {
             } else {
                 boots = this.bootRepository.findByMaterial(material);
             }
-        } else if(type != null) {
+        } else if (type != null) {
             if (size != null) {
                 boots = this.bootRepository.findByTypeAndSize(type, size);
             } else {
                 boots = this.bootRepository.findByType(type);
             }
-        } else if(size != null) {
+        } else if (size != null) {
             boots = this.bootRepository.findBySize(size);            
         } else {
             throw new QueryNotSupportedException("Something went wrong with your search :/(.");
@@ -148,70 +117,4 @@ public class BootController {
         model.addAttribute("newBoot", emptyBoot);
         return "index";
     }
-    /*
-    @GetMapping("/search")
-    public List<Boot> searchBoots(Model model, @RequestParam(required = false) String material, @RequestParam(required = false) BootType type, @RequestParam(required = false) Float size, @RequestParam(required = false, name = "quantity") Integer minQuantity) throws QueryNotSupportedException {
-	/*
-        List<Boot> bootList = this.bootRepository.findByMaterial(material);
-        model.addAttribute("boots", bootList);
-        model.addAttribute("listType", BootType.values());
-        Boot emptyBoot = new Boot();
-        model.addAttribute("newBoot", emptyBoot);
-        return "index";
-        
-        if (Objects.nonNull(material)) {
-            if (Objects.nonNull(type) && Objects.nonNull(size) && Objects.nonNull(minQuantity)) {
-				// call the repository method that accepts a material, type, size, and minimum
-				// quantity
-		return this.bootRepository.findByMaterialAndTypeAndSizeAndQuantityGreaterThan(material, type, size, minQuantity);
-            } else if (Objects.nonNull(type) && Objects.nonNull(size)) {
-				// call the repository method that accepts a material, size, and type
-                return this.bootRepository.findByMaterialAndSizeAndType(material, size, type);
-            } else if (Objects.nonNull(type) && Objects.nonNull(minQuantity)) {
-				// call the repository method that accepts a material, a type, and a minimum
-				// quantity
-                return this.bootRepository.findByMaterialAndTypeAndQuantityGreaterThan(material, type, minQuantity);
-            } else if (Objects.nonNull(type)) {
-				// call the repository method that accepts a material and a type
-                return this.bootRepository.findByMaterialAndType(material, type);
-            } else {
-				// call the repository method that accepts only a material
-                return this.bootRepository.findByMaterial(material);
-            }
-	} else if (Objects.nonNull(type)) {
-            if (Objects.nonNull(size) && Objects.nonNull(minQuantity)) {
-				// call the repository method that accepts a type, size, and minimum quantity
-                return this.bootRepository.findByTypeAndSizeAndQuantityGreaterThan(type, size, minQuantity);
-            } else if (Objects.nonNull(size)) {
-				// call the repository method that accepts a type and a size
-		return this.bootRepository.findByTypeAndSize(type, size);
-            } else if (Objects.nonNull(minQuantity)) {
-				// call the repository method that accepts a type and a minimum quantity
-		return this.bootRepository.findByTypeAndQuantityGreaterThan(type, minQuantity);
-            } else {
-				// call the repository method that accept only a type
-                return this.bootRepository.findByType(type);
-            }
-	} else if (Objects.nonNull(size)) {
-            if (Objects.nonNull(minQuantity)) {
-				// call the repository method that accepts a size and a minimum quantity
-		return this.bootRepository.findBySizeAndQuantityGreaterThan(size, minQuantity);
-            } else {
-				// call the repository method that accepts only a size
-		return this.bootRepository.findBySize(size);
-            }
-	} else if (Objects.nonNull(minQuantity)) {
-			// call the repository method that accepts only a minimum quantity
-            return this.bootRepository.findByQuantityGreaterThan(minQuantity);
-	} else {
-            throw new QueryNotSupportedException("This query is not supported! Try a different combination of search parameters.");
-	}
-        
-    }
-    */
-    /*
-    private Iterable<Boot> getAllBoots() {
-        return this.bootRepository.findAll();
-    }
-    */
 }
--- src/main/java/com/example/bootsBootique/repository/BootRepository.java
+++ src/main/java/com/example/bootsBootique/repository/BootRepository.java
@@ -19,17 +19,10 @@ public interface BootRepository extends CrudRepository<Boot, Integer> {
   public List<Boot> findBySize(Float size);
   public List<Boot> findByMaterial(String material);
   public List<Boot> findByType(BootType type);
-  public List<Boot> findByQuantityGreaterThan(Integer quantity);
 
   public List<Boot> findByMaterialAndType(String material, BootType type);
   public List<Boot> findByMaterialAndSize(String material, Float size);
   public List<Boot> findByTypeAndSize(BootType type, Float size);
-  public List<Boot> findByTypeAndQuantityGreaterThan(BootType type, Integer quantity);
-  public List<Boot> findBySizeAndQuantityGreaterThan(Float size, Integer quantity);
 
   public List<Boot> findByMaterialAndSizeAndType(String material, Float size, BootType type);
-  public List<Boot> findByMaterialAndTypeAndQuantityGreaterThan(String material, BootType type, Integer size);
-  public List<Boot> findByTypeAndSizeAndQuantityGreaterThan(BootType type, Float size, Integer quantity);
-
-  public List<Boot> findByMaterialAndTypeAndSizeAndQuantityGreaterThan(String material, BootType type, Float size, Integer quantity);
 }
